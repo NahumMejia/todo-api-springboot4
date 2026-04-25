39134cb4181d4abead2a028c721b776b5d873ed9
Nahum <nahummejiad@gmail.com>
2026-04-24 22:50:49 -0600

    Fix: Missing @Repository

--- src/main/java/dev/nahum/todo_api/repository/TaskRepository.java
+++ src/main/java/dev/nahum/todo_api/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package dev.nahum.todo_api.repository;
 
 import dev.nahum.todo_api.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task,Long> {
 }
