5aa96aa82aed15b881af6fd74a599c2fbd6480f6
Nahum <nahummejiad@gmail.com>
2026-04-24 23:15:15 -0600

    Modified: TaskController adding the correct mapping

--- src/main/java/dev/nahum/todo_api/controller/TaskController.java
+++ src/main/java/dev/nahum/todo_api/controller/TaskController.java
@@ -4,6 +4,7 @@ import dev.nahum.todo_api.model.Task;
 import dev.nahum.todo_api.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,26 +15,30 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    @PostMapping()
     public Task createTask(Task task){
         return taskService.createTask(task);
     }
 
+    @PutMapping()
     public Task updateTask(Task task){
         return taskService.updateTask(task);
     }
 
+    @DeleteMapping()
     public ResponseEntity<Void> deleteById(Long id){
         taskService.deleteTaskById(id);
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping()
     public List<Task> findAll(){
         return taskService.findAll();
     }
 
+    @GetMapping("/{id}")
     public Task findById(Long id){
         return taskService.findById(id);
     }
-
 
 }
